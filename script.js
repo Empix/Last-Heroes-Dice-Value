82f560438af6c8f7017a14957243d785e2ec74e1
Ádrian <adrian.fr.fe@gmail.com>
2022-08-20 13:38:41 -0400

    feat: change theme

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const resultElement = document.querySelector(".result");
-const attributeInput = document.querySelector("#attribute");
-const diceInput = document.querySelector("#dice");
+const resultElement = document.querySelector('.result');
+const attributeInput = document.querySelector('#attribute');
+const diceInput = document.querySelector('#dice');
 
-attributeInput.addEventListener("input", refreshResultElement);
-diceInput.addEventListener("input", refreshResultElement);
+attributeInput.addEventListener('input', refreshResultElement);
+diceInput.addEventListener('input', refreshResultElement);
 
 function refreshResultElement() {
   const diceValue = parseInt(diceInput.value);
@@ -16,41 +16,32 @@ function refreshResultElement() {
 
 function getResult(dice, attribute) {
   if (isNaN(dice) || isNaN(attribute)) {
-    return "...";
+    return '...';
   }
 
-  let result = "";
   const total = attribute;
   const metade = attribute / 2;
   const quinto = attribute / 5;
 
-  if (dice == 100) {
-    // FALHA CRÍTICA
-    result = `FALHA CRÍTICA`;
-  } else if (dice > total || total == 0) {
-    // FALHA
-    result = `FALHA`;
-  } else if (dice == 1) {
-    // CRÍTICO
-    result = `CRÍTICO`;
+  if (dice === 100) {
+    return `FALHA CRÍTICA`;
+  } else if (dice > total || total === 0) {
+    return `FALHA`;
+  } else if (dice === 1) {
+    return `CRÍTICO`;
   } else if (dice > metade && dice <= total) {
-    // NORMAL
-    result = `NORMAL`;
+    return `NORMAL`;
   } else if (dice > quinto && dice <= metade) {
-    // BOM
-    result = `BOM`;
+    return `BOM`;
   } else if (dice <= quinto && dice > 1) {
-    // EXTREMO
-    result = `EXTREMO`;
+    return `EXTREMO`;
   }
-
-  return result;
 }
 
-// let string = "";
+// let string = '';
 // const attr = 10;
 // string += `Atributo: ${attr}\n`;
 // for (let i = 1; i <= 100; i++) {
-//   string += getResult(i, attr) + " - Dado: " + i + "\n";
+//   string += getResult(i, attr) + ' - Dado: ' + i + '\n';
 // }
 // console.log(string);
